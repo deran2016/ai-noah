f1165a33a03e2cc71a6d1fdfa09a81826629c7ce
INYEOB <deran2016@gmail.com>
2021-10-02 01:58:36 +0900

    add a submit button & Textbox in Quiz.vue

--- src/views/Quiz.vue
+++ src/views/Quiz.vue
@@ -13,20 +13,27 @@
       <template v-slot:default="dialog">
         <v-card>
           <v-card-text>
-            <div class="body-1 pa-3 py-6">
-              이제 문제를 풀어보겠습니다.<br />
-              Noah가 낸 문제를 보고, 맞는 답을 고르세요.<br />
-              기회는 한 번 뿐이니 신중히 생각 후 답을 골라주세요.<br />
-              단어를 선택하면 다음 문제로 넘어갑니다.<br />
+            <br />
+            <Textbox
+              :value="textbox[round][page]"
+            />
+            <div
+              class="px-3 py-5 body-1 text-center"
+              style="width: 25%; margin: 0 auto"
+            >
+              <v-img
+                :src="require('@/assets/noah.png')"
+              />
             </div>
           </v-card-text>
           <v-card-actions class="justify-end">
             <v-btn
               block
+              :disabled="disabled"
               color="primary"
               @click="dialog.value = false; overlay = true"
             >
-              확인
+              준비됐어 {{ countDown > 0 ? `(${countDown})` : '' }}
             </v-btn>
           </v-card-actions>
         </v-card>
@@ -41,30 +48,48 @@
 
     <v-card-text>
       <div
-        class="text-center"
+        class="body-2 text-center"
       >
         {{ (quizIndex + 1) + '/' + (quizs[round].length) }}
       </div>
+      <Textbox
+        style="font-size: 30px; padding: 30px"
+        :value="[quiz.question]"
+      />
       <div
-        class="word"
+        class="px-3 py-5 body-1 text-center"
+        style="width: 40%; margin: 0 auto"
       >
-        {{ quiz.question }}
+        <v-img
+          :src="require('@/assets/noah.png')"
+        />
       </div>
       <v-divider />
     </v-card-text>
 
     <v-card-actions>
       <v-col>
+        <v-col>
+          <v-btn
+            v-for="(item, i) in quiz.answers"
+            :key="i"
+            block
+            depressed
+            class="my-2"
+            :color="selected && selected === item ? 'primary' : 'normal'"
+            @click="selected = item"
+          >
+            {{ item }}
+          </v-btn>
+        </v-col>
         <v-btn
-          v-for="(item, i) in shuffleArr(quiz.answers)"
-          :key="i"
           block
-          outlined
           class="my-2"
           color="primary"
-          @click="checkAnswer(item)"
+          :disabled="!selected"
+          @click="checkAnswer(selected)"
         >
-          {{ item }}
+          선택완료
         </v-btn>
       </v-col>
     </v-card-actions>
@@ -75,16 +100,26 @@
 /* eslint-disable import/no-unresolved */
 import { mapMutations } from 'vuex';
 import Counter from './components/Counter.vue';
+import Textbox from './components/Textbox.vue';
 
 export default {
   components: {
     Counter,
+    Textbox,
   },
 
   data: () => ({
+    countDown: 3,
     dialog: true,
     overlay: false,
+    page: 0,
+    textbox: [[[
+      '이제 문제를 풀어보자!',
+      '내가 문제를 내볼게.',
+      '준비됐어?',
+    ]]],
     quizIndex: 0,
+    selected: '',
     quiz: {
       question: '',
       answers: ['', '', ''],
@@ -110,12 +145,18 @@ export default {
       return this.$store.state.data.experimentType;
     },
 
+    disabled() {
+      return this.countDown > 0;
+    },
+
     round() {
       return this.$store.state.data.round || 0;
     },
   },
 
   mounted() {
+    this.countDownTimer();
+    this.shuffleQuiz();
   },
 
   methods: {
@@ -138,6 +179,14 @@ export default {
       return newArr;
     },
 
+    shuffleQuiz() {
+      this.quizs[this.round].map((item) => {
+        const nitem = item;
+        nitem.answers = this.shuffleArr(item.answers);
+        return nitem;
+      });
+    },
+
     setQuiz() {
       this.quiz = this.quizs[this.round][this.quizIndex];
     },
@@ -151,18 +200,6 @@ export default {
     },
 
     checkAnswer(item) {
-      /** 맞는 답일때만 넘어가려면
-      if (this.quiz.answer === item) {
-        if (this.quizs[this.round].length - 1 === this.quizIndex) {
-          console.log('퀴즈 종료');
-          this.submit();
-        } else {
-          this.pushAnswer(item);
-          this.quizIndex += 1;
-          this.setQuiz();
-        }
-      }
-      */
       this.pushAnswer(item);
       if (this.quizs[this.round].length - 1 === this.quizIndex) {
         console.log('퀴즈 종료');
@@ -170,6 +207,15 @@ export default {
       } else {
         this.quizIndex += 1;
         this.setQuiz();
+      }
+    },
+
+    countDownTimer() {
+      if (this.countDown > 0) {
+        setTimeout(() => {
+          this.countDown -= 1;
+          this.countDownTimer();
+        }, 1000);
       }
     },
   },
