b9058fd8959e8d155262d1cf6391c467b160d959
INYEOB <deran2016@gmail.com>
2021-10-16 20:57:15 +0900

    add texts of Textbox for condition 3 & 4 in Intro.vue

--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -6,7 +6,7 @@
   >
     <v-card-text>
       <Textbox
-        :value="textbox[page]"
+        :value="textbox[parseInt((condition - 1) / 2)][page]"
       />
 
       <div
@@ -27,7 +27,7 @@
         :disabled="disabled"
         @click="next()"
       >
-        {{ page === textbox.length - 1 ? '준비됐어' : '다음' }}
+        {{ page === textbox[parseInt((condition - 1) / 2)].length - 1 ? '준비됐어' : '다음' }}
         {{ countDown > 0 ? `(${countDown})` : '' }}
       </v-btn>
     </v-card-actions>
@@ -47,19 +47,43 @@ export default {
     countDown: 4,
     page: 0,
     textbox: [[
-      '안녕? 반가워.',
-      '난 너의 친구, 노아라고 해.',
+      [
+        '안녕? 반가워.',
+        '난 너의 친구, 노아라고 해.',
+      ],
+      [
+        '오늘은 나와 같이',
+        '기초 불어를 공부해보자.',
+      ],
+      [
+        '불어는 아마 잘 모를테니까,',
+        '우선 간단하게 단어를 읽는',
+        '방법부터 알아볼까?',
+      ],
+      [
+        '재미있겠다! 준비됐어?',
+        '준비됐으면 아래의 버튼을',
+        '눌러줘.',
+      ],
     ], [
-      '오늘은 나와 같이',
-      '기초 불어를 공부해보자.',
-    ], [
-      '불어는 아마 잘 모를테니까,',
-      '우선 간단하게 단어를 읽는',
-      '방법부터 알아볼까?',
-    ], [
-      '재미있겠다! 준비됐어?',
-      '준비됐으면 아래의 버튼을',
-      '눌러줘.',
+      [
+        '안녕하세요? 만나서 반가워요.',
+        '전 오늘의 불어 선생님, 노아라고 해요.',
+      ],
+      [
+        '오늘은 저와 같이',
+        '기초 불어를 공부해봅시다.',
+      ],
+      [
+        '불어는 아마 잘 모를테니까,',
+        '우선 간단하게 단어를 읽는',
+        '방법부터 알아볼까요?',
+      ],
+      [
+        '재미있겠네요! 준비됐나요?',
+        '준비됐으면 아래의 버튼을',
+        '눌러주세요.',
+      ],
     ]],
   }),
 
@@ -97,7 +121,8 @@ export default {
     },
 
     next() {
-      if (this.page >= this.textbox.length - 1) {
+      // eslint-disable-next-line radix
+      if (this.page >= this.textbox[parseInt((this.condition - 1) / 2)].length - 1) {
         this.submit();
       } else {
         this.page += 1;
