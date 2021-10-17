803377287cc1318179e8cfdb4abf3e0948272e45
INYEOB <deran2016@gmail.com>
2021-10-17 17:31:17 +0900

    add MediaList.vue

---
+++ src/views/components/MediaList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="mediaList">
+    <div>
+      <audio
+        v-for="(file, key) in tutorial"
+        :id="file.id"
+        :key="key"
+        :src="`https://docs.google.com/uc?export=open&id=${file.src}`"
+        preload="auto"
+      >
+        <p>Your browser does not support the <code>audio</code> element.</p>
+      </audio>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    tutorial: [
+      { id: 'Bonjour', src: '1-K3w6B4OiVWZGscl0E4COiEBOwCXGXSe' },
+      { id: 'Oui', src: '1-IL_gLDkUhZs1-bplJWwqyuN3ve4mGHm' },
+    ],
+    easy: [],
+    hard: [],
+  }),
+};
+</script>
