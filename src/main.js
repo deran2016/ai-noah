e26f0d5473e3e7fbd32b2f967e7ddc7b6ec368c4
INYEOB <deran2016@gmail.com>
2021-10-22 11:59:02 +0900

    update firebase configures

--- src/main.js
+++ src/main.js
@@ -8,6 +8,18 @@ import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
 
+const firebaseConfig = {
+  apiKey: 'AIzaSyCrG36piiSuGabhU98dC4WXrx0qarOk92M',
+  authDomain: 'ai-noah.firebaseapp.com',
+  databaseURL: 'https://ai-noah-default-rtdb.asia-southeast1.firebasedatabase.app',
+  projectId: 'ai-noah',
+  storageBucket: 'ai-noah.appspot.com',
+  messagingSenderId: '301783147583',
+  appId: '1:301783147583:web:1f0e42191799968076048a',
+  measurementId: 'G-VK6GTRD3SM',
+};
+
+firebase.initializeApp(firebaseConfig);
 Vue.prototype.$firebase = firebase;
 Vue.config.productionTip = false;
 
